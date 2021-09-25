5ea155a43b7fc34f9265a1e5c0d7d59378363c3e
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-09-25 15:17:58 -0500

    update

--- global_settings.py
+++ global_settings.py
@@ -1,7 +1,7 @@
 import os
 
-HOME = "/Users/mingyu/Desktop/technical-analysis"
-WORK_DIR = os.path.join(HOME, "technical-analysis")
+HOME = "/Users/mingyu/Desktop/technical-analysis-playground"
+WORK_DIR = os.path.join(HOME, "technical-analysis-playground")
 DATA_DIR = os.path.join(HOME, "data")
 
 API_KEY = "1KLZRmwL6RGzG8bekmZL"
