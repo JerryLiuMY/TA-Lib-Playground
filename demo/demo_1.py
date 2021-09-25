113d6bf25be66d9afe31c04b800c4ff5009ab0b8
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-09-25 16:33:18 -0500

    update

--- demo/demo_1.py
+++ demo/demo_1.py
@@ -1,21 +1,19 @@
 import pandas_datareader.data as web
 import pandas as pd
 import numpy as np
-from talib import RSI, BBANDS, SMA
+from talib import RSI, BBANDS, SMA, MOM
 import matplotlib.pyplot as plt
 from global_settings import API_KEY
-import seaborn as sns
-sns.set()
 
 
-def get_df(ticker, start, end, max_holding):
+def get_data(ticker, start, end, max_holding):
     data_df = web.DataReader(name=ticker, data_source="quandl", start=start, end=end, api_key=API_KEY).iloc[::-1]
     data_df.dropna(inplace=True)
     close, index = data_df["AdjClose"].values, data_df.index
     data_df["BB_up"], data_df["BB_mid"], data_df["BB_low"] = BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2)
-    data_df["SMA_10"], data_df["SMA_50"] = SMA(close, 10), SMA(close, 50)
-    data_df["RSI"] = RSI(close, timeperiod=14)
     data_df["BBP"] = _get_bbp(data_df)
+    data_df["SMA_10"], data_df["SMA_50"] = SMA(close, 10), SMA(close, 50)
+    data_df["RSI"], data_df["MOM"] = RSI(close, timeperiod=14), MOM(close, timeperiod=14)
 
     holdings_df = pd.DataFrame(index=data_df.index, data={"Holdings": np.array([np.nan] * index.shape[0])})
     holdings_df.loc[((data_df["RSI"] < 30) & (data_df["BBP"] < 0)), "Holdings"] = max_holding
@@ -28,14 +26,14 @@ def get_df(ticker, start, end, max_holding):
     return data_df, holdings_df
 
 
-def plot(data_df, holdings_df):
+def plot_demo(data_df, holdings_df):
     index = data_df.index
     fig, axes = plt.subplots(3, 1, figsize=(12, 8))
 
     # Price
-    axes[0].plot(index, data_df["AdjClose"], label="Adj_Close")
-    axes[0].plot(index, data_df["SMA_10"], label="MA_10")
-    axes[0].plot(index, data_df["SMA_50"], label="MA_50")
+    axes[0].plot_demo(index, data_df["AdjClose"], label="Adj_Close")
+    axes[0].plot_demo(index, data_df["SMA_10"], label="MA_10")
+    axes[0].plot_demo(index, data_df["SMA_50"], label="MA_50")
     axes[0].legend(loc="upper left")
     axes[0].set_xlabel("Date")
     axes[0].set_ylabel("Adj Close")
@@ -47,19 +45,24 @@ def plot(data_df, holdings_df):
         elif order < 0:
             axes[0].scatter(x=day, y=data_df.loc[day, "AdjClose"], color="red")
 
-    # RSI
-    axes[1].plot(index, data_df["RSI"], label="RSI")
+    # RSI & MOM
+    axes[1].plot_demo(index, data_df["RSI"], label="RSI")
     axes[1].fill_between(index, y1=30, y2=70, color="#adccff", alpha=0.3)
-    axes[0].legend(loc="upper left")
+    axes[1].legend(loc="upper left")
     axes[1].set_xlabel("Date")
     axes[1].set_ylabel("RSI")
     axes[1].grid()
 
+    ax_ = axes[1].twinx()
+    ax_.plot_demo(index, data_df["MOM"], label="MOM", color="r", alpha=0.75)
+    ax_.legend(loc="upper right")
+    ax_.set_ylabel("MOM")
+
     # BBANDS
-    axes[2].plot(index, data_df["AdjClose"], label="Adj_Close")
-    axes[2].plot(index, data_df["BB_up"], label="BB_up")
-    axes[2].plot(index, data_df["BB_low"], label="BB_low")
-    axes[2].plot(index, data_df["BB_mid"], label="BB_mid")
+    axes[2].plot_demo(index, data_df["AdjClose"], label="Adj_Close")
+    axes[2].plot_demo(index, data_df["BB_up"], label="BB_up")
+    axes[2].plot_demo(index, data_df["BB_low"], label="BB_low")
+    axes[2].plot_demo(index, data_df["BB_mid"], label="BB_mid")
     axes[2].fill_between(index, y1=data_df["BB_low"], y2=data_df["BB_up"], color="#adccff", alpha=0.3)
     axes[2].set_xlabel("Date")
     axes[2].set_ylabel("Bollinger Bands")
