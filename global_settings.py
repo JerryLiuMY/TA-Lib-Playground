43c288caf0d2be7e1a90d617169fd3ee6c366e5a
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-09-25 23:20:14 -0500

    update

--- global_settings.py
+++ global_settings.py
@@ -1,7 +1,7 @@
 import os
 
 HOME = "/Users/mingyu/Desktop"
-WORK_DIR = os.path.join(HOME, "TA-Lib-playground")
+WORK_DIR = os.path.join(HOME, "TA-Lib-Playground")
 DATA_DIR = os.path.join(HOME, "data")
 
 API_KEY = "1KLZRmwL6RGzG8bekmZL"
